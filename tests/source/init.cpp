622e18b1070d1a5963bc33929d9290636209dd4b
Alexander <Gorbachev2198@gmail.com>
2017-05-19 10:34:14 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,7 +1,67 @@
-#include <Complex.hpp>
-#include <catch.hpp>
+#include "complex.hpp"
+#include "catch.hpp"
 
-SCENARIO ("test"){
-	int i =1;
-	REQUIRE(i == 1);
+
+SCENARIO("test +"){
+  Complex a(5,2);
+  Complex b(3,-3);
+  Complex c(8,-1);
+  Complex temp = a + b;
+  REQUIRE (temp == c);
 }
+
+SCENARIO("test +="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  Complex c(8,-1);
+  a+=b;
+  REQUIRE (a == c);
+}
+
+SCENARIO("test *"){
+  Complex a(5,2);
+  Complex b(3,-3);
+  Complex c(21,-9);
+  Complex temp = a*b;
+  REQUIRE (temp == c);
+}
+
+SCENARIO("test *="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  Complex c(21,-9);
+  a*=b;
+  REQUIRE (a == c);
+} 
+
+SCENARIO("test -"){
+  Complex a(5,2);
+  Complex b(3,-3);
+  Complex c(2,5);
+  Complex temp = a-b;
+  REQUIRE (temp == c);
+}
+
+SCENARIO("test -="){
+  Complex a(5,2);
+  Complex b(3,-3);
+  Complex c(2,5);
+  a-=b;
+  REQUIRE (a == c);
+}
+
+SCENARIO("test /"){
+  Complex a(-1,3);
+  Complex b(1,2);
+  Complex c(1, 1);
+  Complex temp = a/b;
+  REQUIRE (temp == c);
+}
+  
+SCENARIO("test /="){
+  Complex a(-1,3);
+  Complex b(1,2);
+  Complex c(1,1);
+  a/=b;
+  REQUIRE (a == c);  
+}  
